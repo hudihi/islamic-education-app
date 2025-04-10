f3e4105537e5d5db139b7c78712799b2790760c2
Hudihi <Abdillahissa866@gmail.com>
2025-04-10 18:01:58 +0300

    modifying setting

--- baitul/settings.py
+++ baitul/settings.py
@@ -84,8 +84,13 @@ WSGI_APPLICATION = 'baitul.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(conn_max_age=600)
+    'default': dj_database_url.config(
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
+
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
